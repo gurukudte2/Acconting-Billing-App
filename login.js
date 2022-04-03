18d120162c9ae4fe39797a6cf8987779b0fec0e0
Gkcoding <96391168+gurukudte@users.noreply.github.com>
2022-04-03 21:02:24 +0530

    data from Json

--- login.js
+++ login.js
@@ -1,3 +1,4 @@
+//LOGIN-REGISTER ANIMATION
 var button = document.getElementsByClassName("toggle-btn");
 var login = button[0];
 var register = button[1];
@@ -8,8 +9,8 @@ var foranimation = document.getElementById("btn");
 // console.log(loginpage.style.opacity)
 register.addEventListener("click", function(){
     
-    loginpage.style.opacity = "0";
-    registerpage.style.opacity = "1";
+    loginpage.style.display = "none";
+    registerpage.style.display = "block";
     btn.style.translation = ".5s";
     foranimation.style.margin ='0 0 0 90px';
     foranimation.style.width = "121px"
@@ -17,27 +18,87 @@ register.addEventListener("click", function(){
 });
 login.addEventListener("click", function(){
     
-    loginpage.style.opacity = "1";
-    registerpage.style.opacity = "0";
+    loginpage.style.display = "block";
+    registerpage.style.display = "none";
     btn.style.translation = ".5s";
     foranimation.style.margin ='0 0 0 0px';
     foranimation.style.width = "110px"
 
 });
 
+//data from json
+// let xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange=function(){
+//     if (xhttp.readyState==4 && xhttp.status==200){
+//         var data = JSON.parse(xhttp.responseText);
+//         alert(data.username);
+//     }
+//     else{
+//     }
+//     xhttp.open("GET",users.json,true)
+//     xhttp.send();
+// }
 
-//User Auth
-const users =[
-    {username: 'Guru', password: 'gk111'}
-]
-var userName = document.getElementById("username");
-var passWord = document.getElementById("password");
 
-var submit = document.getElementsByClassName("subit-btn");
-console.log(submit);
-submit.addEventListener("click",function() {
+// //User 
+// const users =[
+//     {
+//         username: "guru", 
+//         password: "gk111"
+//     },
+//     {
+//         username:"guru2",
+//         password:"gk112"
+//     }
+// ]
+
+// //login auth
+var submit = document.getElementsByClassName("submit-btn");
+var loginSubmit = submit[0];
+var input = document.getElementsByClassName('input-field');
+loginSubmit.addEventListener("click", function(){
     
-    if (username==users.userName){
-        alert("Please enter");
-    }
-})
+//    alert("hi")
+//     var username = input[0].value;
+//     var password = input[1].value;
+
+    let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange=function(){
+
+        if (xhttp.readyState==4 && xhttp.status==200){
+            var data = JSON.parse(xhttp.responseText);
+            alert(data.username);
+        }
+        else{
+            // alert("no server")
+        }
+        xhttp.open("GET","users.json",true)
+        xhttp.send();
+}
+
+
+    // for (var i = 0; i < users.length;i++)
+    // if (username == users[i].username && password == users[i].password){
+    //     alert("Log In Success")
+    // }
+    // if (password != users[0].password){
+    //     alert("wrong password")
+    // }
+
+});
+
+// //user addition through register
+// var registerSubmit = submit[1];
+// registerSubmit.addEventListener("click", function(){
+
+//     var userName = input[2].value;
+//     var passWord = input[3].value;
+    
+//     var newUser = {};
+//     var usern = userName;
+//     var userp = passWord;
+//     newUser[0].username = "guru3";
+//     nerUser[0].password = "g111";
+//     users.push(newUser);
+//     alert(users)
+// });
